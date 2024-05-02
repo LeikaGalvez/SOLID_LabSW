b0b947205e5a451f88cca1c0c669ed9606cbb15c
LeikaGalvez <142652629+LeikaGalvez@users.noreply.github.com>
2024-05-02 01:04:43 +0000

    fixed SOLID_SRP_1

--- SOLID_SRP_1/PetEat.java
+++ SOLID_SRP_1/PetEat.java
@@ -1,12 +1,6 @@
 package SOLID_SRP_1;
 
 public class PetEat {
-    public Pet pet;
-
-    public PetEat(Pet pet){
-        this.pet=pet;
-    }
-
     public String eat(Pet pet, String food){
         return pet.getOwner() + " is feeding " + pet.getPetName() + " " + food;
     }
--- SOLID_SRP_1/PetMain.java
+++ SOLID_SRP_1/PetMain.java
@@ -3,8 +3,11 @@ package SOLID_SRP_1;
 public class PetMain {
     public static void main(String[] args) {
         Pet pet = new Pet("Whitey", "Leika");
+        PetEat petEat = new PetEat();
+        PetSound petSound = new PetSound();
+
         System.out.println(petEat.eat(pet, "dog food"));
-        System.out.println(makeSound.makeSound(pet, "bark"));
+        System.out.println(petSound.makeSound(pet, "bark"));
 
     }
 }
